1f8eb1c03aa06621bd7fb0fc587dacc391855800
seoyeonKKK <syeon0204@naver.com>
2019-04-12 19:57:12 +0900

    add index query, socket connection

--- smartAD/routes/index.js
+++ smartAD/routes/index.js
@@ -14,21 +14,38 @@ app.get('/', (req, res) => {
 });
 
 io.on("connection", (socket) => {
+  //var d= new Date();
+  //var localTime=d.toLocaleTimeString();
+  //document.getElementById('')
   let _result, result;
   let selectQuery = `SELECT name, url 
-                     FROM ad 
-                     WHERE ad.idx = tag.idx 
+                     FROM ad, tag 
+                     WHERE ad.ad_idx = tag.ad_idx 
                      AND tag.age = ? 
-                     AND (tag.gender = ? OR tag.gender = Null) 
-                     AND (tag.emotion = ? OR tag.emotion = Null)
+                     AND (tag.gender = ? OR tag.gender is Null) 
+                     AND (tag.emotion = ? OR tag.emotion is Null)
                      ORDER BY RAND() LIMIT 1  
-                     `
+                    `
   //client로 보내는 이벤트인 hello
   socket.on("client1", async(data) => {
-    let age = data[0];
     let gender = data[1];
     let emotion = data[2];
-    _result = await db.Query(selectQuery, [age, gender, emotion]);
+    let age;
+    if (parseInt(data[0]) > 0 && parseInt(data[0]) < 13) {
+      age = "112"
+    }
+    else if (parseInt(data[0]) > 12 && parseInt(data[0]) < 19) {
+      age = "1318"
+    }
+    else if (parseInt(data[0]) > 18 && parseInt(data[0]) < 40) {
+      age = "1939"
+    }
+    else if (parseInt(data[0]) > 39) {
+      age = "4069"
+    }
+    _result = await db.query(selectQuery, [age, gender, emotion]);
+    console.log(_result)
+    socket.emit("ad", _result);
   });
   socket.on('Error', function (err) {
     console.log(err);
@@ -38,7 +55,5 @@ io.on("connection", (socket) => {
   });
   socket.on('disconnect', () => {
     console.log("Socket is disconnected!")
-    socket.open();
   });
-  socket.emit("ad", {ad : _result});
 });
