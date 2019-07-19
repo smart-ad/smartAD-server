d98d8e440082cc110efd70a31dbf159d12e7ffb6
DESKTOP-BB1MKS1\user <shinasong@naver.com>
2019-07-19 16:25:14 +0900

    수정

--- smartAD/routes/routes.js
+++ smartAD/routes/routes.js
@@ -5,6 +5,5 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/index', require('./index.js'));
-router.use('/sensor', require('./sensor.js'));
 
 module.exports = router;
